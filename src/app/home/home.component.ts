803bb926395e46005d0413224e675dde8b573581
JuanBarbosaMora <portelnom0308@gmail.com>
2025-02-11 18:12:36 -0500

    Agregando animaciones menu desplegable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,11 +9,32 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  
+
   menuOpen: boolean = false;
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
   toggleMenu(): void {
+    const menu = this.el.nativeElement.querySelector('#containerLinks');
+
+    if (!this.menuOpen) {
+      // Cerrar el menú con la animación de subida
+      this.renderer.removeClass(menu, 'show-menu');
+      this.renderer.addClass(menu, 'hide-menu');
+
+      // Esperar que termine la animación antes de ocultar el menú
+      setTimeout(() => {
+        this.renderer.setStyle(menu, 'display', 'none');
+      }, 300); // Ajusta este tiempo si la animación dura más
+
+    } else {
+      // Mostrar el menú con la animación de caída
+      this.renderer.setStyle(menu, 'display', 'flex');
+      this.renderer.removeClass(menu, 'hide-menu');
+      this.renderer.addClass(menu, 'show-menu');
+    }
+
+    // Cambiar el estado del menú
     this.menuOpen = !this.menuOpen;
   }
 }
-
