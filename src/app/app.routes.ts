bbf807fe8f220783f0ae82f40aa73ec0327dd92f
JuanBarbosaMora <portelnom0308@gmail.com>
2025-02-26 21:48:33 -0500

    Slider complete, adding projects component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-
-    /**
-    * ? Manejo paginas no encontradas
-    */
-   { path: '**', component: NotFoundPageComponent }
+    { path: 'projects', component: ProjectsComponent },
+    { path: '**', component: NotFoundPageComponent }
 ];
